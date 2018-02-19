d747295fd7709c682e8c250e8cf199256dbd7685
Brett F <brettfox11@gmail.com>
2018-02-18 19:27:27 -0600

    Backend and frontend hooked up succesfully for monsters index

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,17 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(){
-    return [{
-      name: 'Sparkachu',
-      level: 3
-    },{
-      name: 'Charember',
-      level: 29
-    }, {
-      name: 'Rhauk',
-      level: 5
-    }, {
-      name: 'Poodle',
-      level: 9
-    }]}
+    return this.store.findAll('monster');
+  }
 });
